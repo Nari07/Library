9dbeb27b1cd21d3541f037f4f1bf471f161c5dbe
Nari07 <mickinguyen12@gmail.com>
2023-08-21 00:00:09 -0500

    update js

---
+++ script.js
@@ -0,0 +1,60 @@
+const titleInput = document.getElementById('titleInput');
+const authorInput = document.getElementById('authorInput');
+const pagesInput = document.getElementById('pagesInput');
+const readInput = document.getElementById('readInput');
+const displayBooks = document.getElementById('displayBooks');
+const submit = document.getElementById('submit');
+
+let myLibrary = [];
+
+function Book(title, author, pages, read){
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary(book){
+  myLibrary.push(book);
+  return myLibrary;
+}
+  
+submit.addEventListener('click', (e) => {
+  e.preventDefault();
+  let title = titleInput.value;
+  let author = authorInput.value;
+  let pages = pagesInput.value;
+  let read;
+    if (readInput.checked === false){
+      read = 'not read';
+    } else if (readInput.checked === true){
+      read = 'read';
+    }
+  
+  let book = new Book(title, author, pages, read);
+ 	addBookToLibrary(book);
+  clearInputs();	
+  console.log(myLibrary);
+  
+});
+
+function clearInputs(){
+  titleInput.value = '';
+  authorInput.value ='';
+  pagesInput.value='';
+  readInput.checked = false;
+
+}
+
+let (i = 0; i < myLibrary.length; i++){
+	let bookCard = document.createElement(div);
+  let bookInfo = document.createTextNode(i);
+  displayBooks.appendChild(bookCard);
+  bookCard.appendChild(bookInfo);
+}
+  
+/* greetingOutput.innerText = `Hello ${name.value}`;
+}) 
+popup how?
+card how?
+*/
